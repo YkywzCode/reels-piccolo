b346a8497e7c71a1955e02e8f138530b8f3e3d95
irfanykywz <irfanyouth@gmail.com>
2025-04-24 22:21:11 +0700

    commit

---
+++ updater/index-debug.php
@@ -0,0 +1,7 @@
+[app]
+version=1.0
+file=http://localhost/reels-piccolo/api/updater/app.zip
+
+[core]
+version=1.0
+file=http://localhost/reels-piccolo/api/updater/core.zip
---
+++ updater/index.php
@@ -0,0 +1,7 @@
+[app]
+version=1.0
+file=https://google.com
+
+[core]
+version=1.0
+file=https://raw.githubusercontent.com/YkywzCode/reels-piccolo/refs/heads/main/updater/core.zip
